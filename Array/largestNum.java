1173c55ea9bf3e43e717ad2addad9fc1217f4e43
Vivekjha-10 <jhavivekkumar81@gmail.com>
2025-09-22 15:25:20 +0530

    largest smallest in array

---
+++ Array/largestNum.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class LargestNum {
+    public static int largest(int numbers[]) {
+        int largest = Integer.MIN_VALUE; 
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+
+    public static int smallest(int numbers[]) {
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < smallest) {
+                smallest = numbers[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 3, 8, 5, 6, 7};
+        System.out.println("Largest number is: " + largest(numbers));
+        System.out.println("Smallest number is: " + smallest(numbers));
+    }
+}
